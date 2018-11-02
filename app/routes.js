268766c19b97be7a0217e2af9436406fa056b4d7
mullinww <winstonmullings@gmail.com>
2018-11-02 15:16:44 +0000

    Initial commit

---
+++ app/routes.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const router = express.Router()
+
+// Add your routes here - above the module.exports line
+
+router.post("/juggling-balls-answer", function(req,res){
+
+var jugglingBalls = req.session.data['juggling-balls']
+console.log(jugglingBalls)
+if (jugglingBalls == "3 or more"){
+
+    res.redirect("/juggling-trick")
+} else {
+  res.redirect("/ineligible")
+}
+
+})
+
+module.exports = router
